e9197c93335294f5b0af3246ebc6f0bff0f70007
Mark Otuya <markotuya0@gmail.com>
2022-11-04 02:21:07 +0100

    update

--- db/migrate/20221102183102_create_comments.rb
+++ db/migrate/20221102183102_create_comments.rb
@@ -5,7 +5,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
       create_table :comments do |t|
         t.references :author, index: true, foreign_key: { to_table: :users }
         t.references :post, index: true, foreign_key: true
-        t.string :text
+        t.text :text
   
         t.timestamps
       end
