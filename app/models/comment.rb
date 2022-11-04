ac00cace3aa5fc6cf4d9f23b6c4f782208571ccf
Mark Otuya <markotuya0@gmail.com>
2022-11-04 23:58:24 +0100

    data insertion

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
-end
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, class_name: 'Post'
+  
+    after_save :update_post_comments_counter
+  
+    def update_post_comments_counter
+      post.increment!(:comments_counter)
+    end
+  end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-end
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+  
+    after_save :update_user_post_counter
+  
+    def five_most_recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    def update_user_post_counter
+      author.increment!(:posts_counter)
+    end
+  end
