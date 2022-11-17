1ebfdd687a30ca9a44b2e6393bb7904ba2921389
Mark Otuya <markotuya0@gmail.com>
2022-11-17 23:18:00 +0100

    rspec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  # index
+  describe 'GET #index' do
+    before(:example) do
+      get '/users'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'renders placeholder for the response body' do
+      expect(response.body).to include('Here is a list of all users')
+    end
+  end
+
+  # show
+  describe 'GET show' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'renders placeholder for the response body' do
+      expect(response.body).to include('Here is a list of one user fetched by their id')
+    end
   end
 end
