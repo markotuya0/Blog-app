e98e8696e2b0c8aee7d5922c16b7cee3b5e4a17d
Mark Otuya <markotuya0@gmail.com>
2022-11-19 18:01:09 +0100

    generated views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
+    @post_text = @post.text
+    @author = @post.author.name
+    @comments = @post.comments
+    @comments_count = @post.comments.length
+    @likes_count = @post.likes.length
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,26 +2,26 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  after_save :update_posts_counter
 
-  after_initialize :initial
-  after_save :update_user_post_counter
-
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def five_most_recent_comments
+  def recent_five
     comments.order(created_at: :desc).limit(5)
-  end
-
-  def update_user_post_counter
-    author.increment!(:posts_counter)
   end
 
   private
 
-  def initial
-    self.comments_counter ||= 0
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  after_initialize :init
+
+  def init
     self.likes_counter ||= 0
+    self.comments_counter ||= 0
   end
 end
