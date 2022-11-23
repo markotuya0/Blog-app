1b625ff50074154412e80f568d02686cd63401e7
Mark Otuya <markotuya0@gmail.com>
2022-11-23 12:26:14 +0100

    test presence of a users profile photo

---
+++ spec/views/users/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'User show', type: :feature do
+  before(:example) do
+    @user = User.create(name: 'Elon Musk', photo: 'link/goes/here', bio: 'this is my bio')
+    User.create(name: 'Diamond Platnumz', photo: 'link/goes/here', bio: 'this is my bio')
+
+    visit users_path
+  end
+
+  it 'renders photo of the user' do
+    expect(page.html).to include(@user.photo)
+  end
+end
