562f91324d83339fd785ea14395e527cadbd4d38
Mark Otuya <markotuya0@gmail.com>
2022-11-23 12:32:04 +0100

    Test the number of posts written by a given user

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -9,8 +9,12 @@ RSpec.describe 'User show', type: :feature do
   it 'renders photo of the user' do
     expect(page.html).to include(@user.photo)
   end
-  
+
   it 'show username of a given user' do
     expect(page).to have_content('Elon Musk')
+  end
+
+  it 'shows number of posts written by agiven user' do
+    expect(page).to have_content(0)
   end
 end
