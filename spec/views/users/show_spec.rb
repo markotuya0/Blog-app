7d134fa0c7cb5920a2b4c054b048766486314d9b
Mark Otuya <markotuya0@gmail.com>
2022-11-23 12:29:56 +0100

    test the presence of username

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe 'User show', type: :feature do
   it 'renders photo of the user' do
     expect(page.html).to include(@user.photo)
   end
+  
+  it 'show username of a given user' do
+    expect(page).to have_content('Elon Musk')
+  end
 end
