f7818495885889b0c3a3c98572b60ea6bb378239
Mark Otuya <markotuya0@gmail.com>
2022-11-24 21:45:52 +0100

    add test for post title

---
+++ spec/views/posts/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'User post', type: :feature do
+  before(:example) do
+    @user = User.create(name: 'Elon Musk', photo: 'link/goes/here', bio: 'this is my bio')
+    @post_one = Post.create(title: 'My first post', text: 'Body text of the post', author_id: @user.id)
+    Post.create(title: 'My second post', text: 'Body text of the post', author_id: @user.id)
+    Post.create(title: 'My third post', text: 'Body text of the post', author_id: @user.id)
+    Post.create(title: 'My fourth post', text: 'Body text of the post', author_id: @user.id)
+
+    visit user_posts_path(@user, @post_one.id)
+  end
+
+  it 'renders posts title ' do
+    expect(page.html).to include(@post_one.title)
+  end
+end
