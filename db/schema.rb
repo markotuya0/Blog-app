6bd6e63d2436b627acada4abce2740d0cf3ac139
Mark Otuya <markotuya0@gmail.com>
2022-11-26 11:46:11 +0100

    add devise functionalities

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_183102) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_101418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.bigint "author_id"
     t.bigint "post_id"
-    t.string "text"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
@@ -35,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_183102) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id"
-    t.string "title"
-    t.string "text"
+    t.text "title"
+    t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
@@ -51,6 +51,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_183102) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
